80c120daf49e83604a3b1546c60bfd2daff7efcf
vaal <whitebaal@gmail.com>
2015-04-25 13:02:57 -0300

    Corrected insertion errors. Changed date types to string in table Vol.

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
 
 /* Test  Dataset insertion  */
 
-INSERT INTO Aeroport VALUES ( 'ybc', '', 'BAGOTVILLE','QC');
+INSERT INTO Aeroport VALUES ( 'YBC', '', 'BAGOTVILLE','QC');
 INSERT INTO Aeroport VALUES ( 'YYC', 'Jean Chretien', 'BAGOTVILLE', 'alberta');
 INSERT INTO Aeroport VALUES ( 'YYZ', 'LeBlanc', 'Charlottetown', 'PE');
 select * from Aeroport;
@@ -16,17 +16,17 @@ INSERT INTO Avion VALUES (1, 'CRJ200', '12','30');
 INSERT INTO Avion VALUES (2, 'SpeedFire', '120', '1');
 select * from Avion;
 
-INSERT INTO Vol VALUES (0 , 'ybc', 'YYC', to_date(  '18:30'  , 'hh24:mi'), to_date( '20:30' , 'hh24:mi'), 0 );
-INSERT INTO Vol VALUES (1 , 'YYC', 'YYZ', to_date(  '18:30'  , 'hh24:mi'), to_date( '20:30' , 'hh24:mi'), 1 );
-INSERT INTO Vol VALUES (2 , 'YYC', 'Ybc', to_date(  '18:30'  , 'hh24:mi'), to_date( '20:30' , 'hh24:mi'), 0 );
+INSERT INTO Vol VALUES ('0' , 'YBC', 'YYC', '18:30', '20:30', 0 );
+INSERT INTO Vol VALUES ('1' , 'YYC', 'YYZ', '18:30', '20:30', 1 );
+INSERT INTO Vol VALUES ('2' , 'YYC', 'YBC', '18:30', '20:30', 0 );
 select * from Vol;
 
-INSERT INTO Affectation VALUES ('0',to_date(  '27/04/2015' , 'dd/mm/yyyy'), '0', '0','');
-INSERT INTO Affectation VALUES ('1',to_date(  '27/04/2015' , 'dd/mm/yyyy'), '2', '1','');
-INSERT INTO Affectation VALUES ('2',to_date(  '28/04/2015' , 'dd/mm/yyyy'), '2', '1','');
+INSERT INTO Affectation VALUES (0, to_date(  '27/04/2015' , 'dd/mm/yyyy'), '0', '0', 0);
+INSERT INTO Affectation VALUES (1, to_date(  '27/04/2015' , 'dd/mm/yyyy'), '2', '1', 0);
+INSERT INTO Affectation VALUES (2, to_date(  '28/04/2015' , 'dd/mm/yyyy'), '2', '1', 0);
 select * from Affectation;
 
 
-INSERT INTO Reservation VALUES ('0', '0', '3', 'Toto Rigolo' );
+INSERT INTO Reservation VALUES (0, 0, '3', 'Toto Rigolo' );
 select * from Reservation;
 
